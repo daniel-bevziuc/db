801752eeaeb8a91722fb0f9eadf34db633e886fd
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-02-08 10:21:49 +0000

    Updates

---
+++ precache-manifest.7592d68d641a45d97c7ee8791c224db2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2509ee191fcef182f4d7b24ccdbc38a3",
+    "url": "/db/static/media/tesla.2509ee19.png"
+  },
+  {
+    "revision": "6ebd2adb8678b5f9c065",
+    "url": "/db/static/css/main.653d2b6b.chunk.css"
+  },
+  {
+    "revision": "e2ed64b2f92d874aac73",
+    "url": "/db/static/js/1.e2ed64b2.chunk.js"
+  },
+  {
+    "revision": "383872b62fceb0a97fae",
+    "url": "/db/static/js/runtime~main.383872b6.js"
+  },
+  {
+    "revision": "72123fc88b060889ce1f4a69180ddbb4",
+    "url": "/db/static/media/logo.72123fc8.svg"
+  },
+  {
+    "revision": "6ebd2adb8678b5f9c065",
+    "url": "/db/static/js/main.6ebd2adb.chunk.js"
+  },
+  {
+    "revision": "50a95f56827ec91dedc740efa262ce23",
+    "url": "/db/static/media/carton.50a95f56.png"
+  },
+  {
+    "revision": "cc29c8dd3ea58e48625735586ab21141",
+    "url": "/db/static/media/part.cc29c8dd.png"
+  },
+  {
+    "revision": "59692f456c838e0ba727b3421dab01cf",
+    "url": "/db/static/media/tech-woman.59692f45.png"
+  },
+  {
+    "revision": "12e65b4f8aed95db5f57dca42588bb54",
+    "url": "/db/static/media/bg.12e65b4f.png"
+  },
+  {
+    "revision": "e89eb0a0019c0df7bb1544cebe0591b4",
+    "url": "/db/index.html"
+  }
+];
