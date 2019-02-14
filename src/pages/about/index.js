0d3a4844d4f62cacdcef39a2a17fbba3da0163bc
Daniel Bevziuc <bevziuc.daniel@gmail.com>
2019-02-14 13:44:23 +0000

    update

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -9,6 +9,7 @@ class About extends Component {
   render() {
     return (
       <Fragment>
+        
         <div className="tag-text-top">
           <span className="tag-content">&#60;html&#62;</span>
           <span className="tag-content tag-content-with-spacing">&#60;body&#62;</span>
